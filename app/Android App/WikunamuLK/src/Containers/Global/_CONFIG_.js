4ae2992fd2de59b87f1357a89e7b7ea04b20376f
Akila Devinda <akila_devinda@Akilas-MacBook-Pro.local>
2019-01-31 12:23:09 +0530

    Change to LIVE API Link URL - Config

--- app/Android App/WikunamuLK/src/Containers/Global/_CONFIG_.js
+++ app/Android App/WikunamuLK/src/Containers/Global/_CONFIG_.js
@@ -14,36 +14,37 @@
  /**
  * Primary API LINKS -----------------------------------------------------
  */
+//Old One const USER_REGISTER_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/register.php';
 
 //Login URL Fro All Users
-const USER_LOGIN_URL = 'http://10.0.2.2/API/post/login.php';
+const USER_LOGIN_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/login.php';
 
 //New User Register For All Users
-const USER_REGISTER_URL = 'http://10.0.2.2/API/post/register.php';
+const USER_REGISTER_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/register.php';
 
 //User Details For Main Profile Screen
-const  USER_PROFILE_URL  = 'http://10.0.2.2/API/post/user-details.php';
+const  USER_PROFILE_URL  = 'https://wikunamulkapp.000webhostapp.com/API/post/user-details.php';
 
 //User Detail Link For Update Screen
-const USER_PROFILE_URL_SEC = 'http://10.0.2.2/API/post/user-detail-second.php';
+const USER_PROFILE_URL_SEC = 'https://wikunamulkapp.000webhostapp.com/API/post/user-detail-second.php';
 
 //User Detail Update URL Link
-const UPDATE_DETAILS_URL = 'http://10.0.2.2/API/post/update-user.php';
+const UPDATE_DETAILS_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/update-user.php';
 
 //Post Ad Live API Link
-const USER_POSTAD_URL = 'http://10.0.2.2/API/post/post-ad.php';
+const USER_POSTAD_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/post-ad.php';
 
 //Get All Ad Details
-const GET_ADDETAILS_URL = 'http://10.0.2.2/API/post/get-ad-details.php'
+const GET_ADDETAILS_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/get-ad-details.php'
 
 //Delete User Account From Database
-const DELETE_USERACC_URL = 'http://10.0.2.2/API/post/delete-user.php'
+const DELETE_USERACC_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/delete-user.php'
 
 //Delete Ad Details from Database
-const DELETE_AD_URL = 'http://10.0.2.2/API/post/delete-ad.php'
+const DELETE_AD_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/delete-ad.php'
 
 //Get Category Details
-const GET_CATDETAILS_URL = 'http://10.0.2.2/API/post/get-category-details.php'
+const GET_CATDETAILS_URL = 'https://wikunamulkapp.000webhostapp.com/API/post/get-category-details.php'
 
 
 
